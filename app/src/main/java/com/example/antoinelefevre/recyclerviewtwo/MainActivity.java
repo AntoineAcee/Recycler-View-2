862b8a337702d1f6625819d218b8760c3ac527f0
SoDRUNK <antoine.lefevre@supinternet.fr>
2019-02-10 19:14:10 +0100

    change default value

--- app/src/main/java/com/example/antoinelefevre/recyclerviewtwo/MainActivity.java
+++ app/src/main/java/com/example/antoinelefevre/recyclerviewtwo/MainActivity.java
@@ -29,32 +29,30 @@ public class MainActivity extends AppCompatActivity {
         ButterKnife.bind(this);
 
         ArrayList<File> files = new ArrayList<>();
-        try { date = dateFormat.parse("12/01/2019"); } catch (ParseException e) {}
-        files.add(new File("Bill Report", "You", new Timestamp(date.getTime())));
+        try { date = dateFormat.parse("20/02/2018"); } catch (ParseException e) {}
+        files.add(new File("Bill Report", "You", new Timestamp(date.getTime()), File.FileType.EXCEL));
+
+        try { date = dateFormat.parse("12/02/2018"); } catch (ParseException e) {}
         files.add(new File("Diagnostics Reports", "Ayush Kumar", new Timestamp(date.getTime()), File.FileType.EXCEL));
+
+        try { date = dateFormat.parse("09/02/2018"); } catch (ParseException e) {}
         files.add(new File("Design Effort", "Ayush Kumar", new Timestamp(date.getTime()), File.FileType.PDF));
 
-        try { date = dateFormat.parse("12/07/2019"); } catch (ParseException e) {}
-        files.add(new File("Lab Tests", "Samar Mahajan", new Timestamp(date.getTime()), File.FileType.WORD));
-        files.add(new File("Lab Tests", "Samar Mahajan", new Timestamp(date.getTime()), File.FileType.WORD));
-        files.add(new File("Lab Tests", "Samar Mahajan", new Timestamp(date.getTime()), File.FileType.WORD));
-        files.add(new File("Lab Tests", "Samar Mahajan", new Timestamp(date.getTime()), File.FileType.WORD));
+        try { date = dateFormat.parse("12/01/2018"); } catch (ParseException e) {}
+        files.add(new File("Lab Tests", "Samar Mahajan", new Timestamp(date.getTime()), File.FileType.PDF));
 
+        try { date = dateFormat.parse("18/01/2018"); } catch (ParseException e) {}
+        files.add(new File("Medication Guidlines", "Ayush Kumar", new Timestamp(date.getTime()), File.FileType.WORD));
 
+        try { date = dateFormat.parse("14/01/2018"); } catch (ParseException e) {}
+        files.add(new File("Pill Reports", "Rajesh Goel", new Timestamp(date.getTime()), File.FileType.EXCEL));
 
-        try { date = dateFormat.parse("12/10/2019"); } catch (ParseException e) {}
-        files.add(new File("Lab Tests", "Samar Mahajan", new Timestamp(date.getTime()), File.FileType.WORD));
-        files.add(new File("Lab Tests", "Samar Mahajan", new Timestamp(date.getTime()), File.FileType.WORD));
-        files.add(new File("Lab Tests", "Samar Mahajan", new Timestamp(date.getTime()), File.FileType.WORD));
-        files.add(new File("Lab Tests", "Samar Mahajan", new Timestamp(date.getTime()), File.FileType.WORD));
+        try { date = dateFormat.parse("01/01/2018"); } catch (ParseException e) {}
+        files.add(new File("Medical Card", "Vikash Sexena", new Timestamp(date.getTime()), File.FileType.WORD));
 
+        try { date = dateFormat.parse("20/01/2018"); } catch (ParseException e) {}
+        files.add(new File("Record for Ayush Kushwaha", "Ayush Goel", new Timestamp(date.getTime()), File.FileType.PDF));
 
-
-        try { date = dateFormat.parse("11/01/2019"); } catch (ParseException e) {}
-        files.add(new File("Lab Tests", "Samar Mahajan", new Timestamp(date.getTime()), File.FileType.WORD));
-        files.add(new File("Lab Tests", "Samar Mahajan", new Timestamp(date.getTime()), File.FileType.WORD));
-        files.add(new File("Lab Tests", "Samar Mahajan", new Timestamp(date.getTime()), File.FileType.WORD));
-        files.add(new File("Lab Tests", "Samar Mahajan", new Timestamp(date.getTime()), File.FileType.WORD));
 
         FileAdapter fileAdapter = new FileAdapter(files);
         recyclerView.setAdapter(fileAdapter);
